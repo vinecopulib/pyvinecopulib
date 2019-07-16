5e7f7ddf4e5f054efe5fc91204d5687b2c8dfa04
Thibault Vatter <thibault.vatter@gmail.com>
2019-07-16 11:08:52 -0400

    use vinecopulib as a submodule

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -1,6 +1,4 @@
-include(cmake/FindEigen3.cmake            REQUIRED)
+include(cmake/findEigen3.cmake            REQUIRED)
 find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
 
-set(wdm_INCLUDE_DIRS "lib/wdm/include")
-set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS} ${wdm_INCLUDE_DIRS})
