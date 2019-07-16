097cd553dbd224f54068895b70cb4dc5d4ce60d8
Thibault Vatter <thibault.vatter@gmail.com>
2019-07-16 04:53:07 -0400

    use submodules again

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -2,6 +2,6 @@ include(cmake/findEigen3.cmake            REQUIRED)
 message(STATUS "Eigen3 version: ${EIGEN3_VERSION}")
 find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
-find_package(wdm                          REQUIRED)
 
+set(wdm_INCLUDE_DIRS "lib/wdm/include")
 set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS} ${wdm_INCLUDE_DIRS})
