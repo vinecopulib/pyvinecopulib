d03c59fac0f7b9fc6b4628286398ad4ab559be68
Thibault Vatter <thibault.vatter@gmail.com>
2019-07-16 03:48:05 -0400

    try git modules

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
+#include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pybind11/eigen.h>
-#include "vinecopulib.hpp"
+#include <vinecopulib.hpp>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(pyvinecopulib, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
+PYBIND11_MODULE(pyvinecopulib, pv) {
+  pv.doc() = R"pbdoc(
+        Pyvinecopulib library
         -----------------------
 
         .. currentmodule:: pyvinecopulib
@@ -15,26 +15,19 @@ PYBIND11_MODULE(pyvinecopulib, m) {
         .. autosummary::
            :toctree: _generate
 
-           add
-           subtract
+           simulate_uniform
     )pbdoc";
 
-    m.def("simulate_uniform", &vinecopulib::tools_stats::simulate_uniform, 
-        R"pbdoc(
-        Add two numbers
+  pv.def("simulate_uniform", &vinecopulib::tools_stats::simulate_uniform,
+         R"pbdoc(
+        Simulate uniform random numbers.
 
-        Some other explanation about the add function.
+        Simulate a matrix of random numbers, with an option to get quasi random
+        numbers or a seed.
     )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
 #ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
+  pv.attr("__version__") = VERSION_INFO;
 #else
-    m.attr("__version__") = "dev";
+  pv.attr("__version__") = "dev";
 #endif
 }
