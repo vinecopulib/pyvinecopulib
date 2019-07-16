f81f73a4971790e15c2ba467d60961790b9dcae5
tnagler <thomas.nagler@tum.de>
2019-07-16 15:37:43 +0200

    fix tests

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -1,4 +1,4 @@
-include(cmake/FindEigen3.cmake       REQUIRED)
+include(cmake/FindEigen3.cmake            REQUIRED)
 find_package(Boost 1.56                   REQUIRED)
 find_package(Threads                      REQUIRED)
 
