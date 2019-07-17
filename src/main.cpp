ae84b49349ee8de3fe6d92ec4213aa3c8523c34c
Thibault Vatter <thibault.vatter@gmail.com>
2019-07-17 09:51:50 -0400

    add cool stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <vinecopulib.hpp>
 
 namespace py = pybind11;
+using namespace vinecopulib;
 
 PYBIND11_MODULE(pyvinecopulib, pv) {
   pv.doc() = R"pbdoc(
@@ -18,7 +19,92 @@ PYBIND11_MODULE(pyvinecopulib, pv) {
            simulate_uniform
     )pbdoc";
 
-  pv.def("simulate_uniform", &vinecopulib::tools_stats::simulate_uniform,
+  py::module pv_bicop_families = pv.def_submodule(
+      "bicop_families", "A submodule of 'pyvinecopulib' with convenience "
+                        "definitions for bivariate families");
+
+  py::enum_<BicopFamily>(pv, "BicopFamily", py::arithmetic())
+      .value("indep", BicopFamily::indep)
+      .value("gaussian", BicopFamily::gaussian)
+      .value("student", BicopFamily::student)
+      .value("clayton", BicopFamily::clayton)
+      .value("gumbel", BicopFamily::gumbel)
+      .value("frank", BicopFamily::frank)
+      .value("joe", BicopFamily::joe)
+      .value("bb1", BicopFamily::bb1)
+      .value("bb6", BicopFamily::bb6)
+      .value("bb7", BicopFamily::bb7)
+      .value("bb8", BicopFamily::bb8)
+      .value("tll", BicopFamily::tll);
+
+  pv_bicop_families.attr("all") = bicop_families::all;
+  pv_bicop_families.attr("parametric") = bicop_families::parametric;
+  pv_bicop_families.attr("nonparametric") = bicop_families::nonparametric;
+  pv_bicop_families.attr("one_par") = bicop_families::one_par;
+  pv_bicop_families.attr("two_par") = bicop_families::two_par;
+  pv_bicop_families.attr("elliptical") = bicop_families::elliptical;
+  pv_bicop_families.attr("archimedean") = bicop_families::archimedean;
+  pv_bicop_families.attr("bb") = bicop_families::bb;
+  pv_bicop_families.attr("rotationless") = bicop_families::rotationless;
+  pv_bicop_families.attr("lt") = bicop_families::lt;
+  pv_bicop_families.attr("ut") = bicop_families::ut;
+  pv_bicop_families.attr("itau") = bicop_families::itau;
+  pv_bicop_families.attr("flip_by_rotation") = bicop_families::flip_by_rotation;
+
+  py::class_<FitControlsBicop>(pv, "FitControlsBicop")
+      .def(py::init<std::vector<BicopFamily>, std::string, std::string, double,
+                    std::string, const Eigen::VectorXd &, double, bool,
+                    size_t>(),
+           "creates the controls for fitting bivariate copula models.",
+           py::arg("family_set") = bicop_families::all,
+           py::arg("parametric_method") = "mle",
+           py::arg("nonparametric_method") = "quadratic",
+           py::arg("nonparametric_mult") = 1.0,
+           py::arg("selection_criterion") = "bic",
+           py::arg("weights") = Eigen::VectorXd(), py::arg("psi0") = 0.9,
+           py::arg("preselect_families") = true, py::arg("num_threads") = 1)
+      /*      .def(py::init<std::string>(), */
+      //      "creates default controls except for the parameteric method.",
+      //      py::arg("parametric_method"))
+      // .def(py::init<std::string, double>(),
+      //      "creates default controls except for the nonparametric method.",
+      /* py::arg("nonparametric_method"), py::arg("mult") = 1.0) */
+      .def_property("family_set", &FitControlsBicop::get_family_set,
+                    &FitControlsBicop::set_family_set)
+      .def_property("parametric_method",
+                    &FitControlsBicop::get_parametric_method,
+                    &FitControlsBicop::set_parametric_method)
+      .def_property("nonparametric_method",
+                    &FitControlsBicop::get_nonparametric_method,
+                    &FitControlsBicop::set_nonparametric_method)
+      .def_property("nonparametric_mult",
+                    &FitControlsBicop::get_nonparametric_mult,
+                    &FitControlsBicop::set_nonparametric_method)
+      .def_property("selection_criterion",
+                    &FitControlsBicop::get_selection_criterion,
+                    &FitControlsBicop::set_selection_criterion)
+      .def_property("weights", &FitControlsBicop::get_weights,
+                    &FitControlsBicop::set_weights)
+      .def_property("psi0", &FitControlsBicop::get_psi0,
+                    &FitControlsBicop::set_psi0)
+      .def_property("preselect_families",
+                    &FitControlsBicop::get_preselect_families,
+                    &FitControlsBicop::set_preselect_families)
+      .def_property("num_threads", &FitControlsBicop::get_num_threads,
+                    &FitControlsBicop::set_num_threads);
+
+  py::class_<Bicop> bicop(pv, "Bicop");
+  bicop.def(py::init<const BicopFamily, const int, const Eigen::MatrixXd &>(),
+            "creates a specific bivariate copula model.",
+            py::arg("family") = BicopFamily::indep, py::arg("rotation") = 0,
+            py::arg("parameters") = Eigen::MatrixXd());
+  bicop.def_property_readonly("rotation", &Bicop::get_rotation);
+  bicop.def_property_readonly("parameters", &Bicop::get_parameters);
+  bicop.def_property_readonly("family", &Bicop::get_family);
+  bicop.def("__repr__", [](const Bicop &cop) {
+    return "<Bicop, family = " + cop.str() + ">";
+  });
+  pv.def("simulate_uniform", &tools_stats::simulate_uniform,
          R"pbdoc(
         Simulate uniform random numbers.
 
@@ -26,27 +112,6 @@ PYBIND11_MODULE(pyvinecopulib, pv) {
         numbers or a seed.
     )pbdoc");
 
-  py::enum_<vinecopulib::BicopFamily>(pv, "BicopFamily", py::arithmetic())
-      .value("indep", vinecopulib::BicopFamily::indep)
-      .value("gaussian", vinecopulib::BicopFamily::gaussian)
-      .value("student", vinecopulib::BicopFamily::student)
-      .value("clayton", vinecopulib::BicopFamily::clayton)
-      .value("gumbel", vinecopulib::BicopFamily::gumbel)
-      .value("frank", vinecopulib::BicopFamily::frank)
-      .value("joe", vinecopulib::BicopFamily::joe)
-      .value("bb1", vinecopulib::BicopFamily::bb1)
-      .value("bb6", vinecopulib::BicopFamily::bb6)
-      .value("bb7", vinecopulib::BicopFamily::bb7)
-      .value("bb8", vinecopulib::BicopFamily::bb8)
-      .value("tll", vinecopulib::BicopFamily::tll);
-
-  py::class_<vinecopulib::Bicop> bicop(pv, "Bicop");
-  bicop.def(py::init<const vinecopulib::BicopFamily, const int,
-                     const Eigen::MatrixXd &>());
-  bicop.def_property_readonly("rotation", &vinecopulib::Bicop::get_rotation);
-  bicop.def_property_readonly("parameters",
-                              &vinecopulib::Bicop::get_parameters);
-  bicop.def_property_readonly("family", &vinecopulib::Bicop::get_family);
 #ifdef VERSION_INFO
   pv.attr("__version__") = VERSION_INFO;
 #else
