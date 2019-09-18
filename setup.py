d0cf56cc2e04558570995b03bc9fb47f4957bb08
Thibault Vatter <thibault.vatter@gmail.com>
2019-09-18 14:53:10 -0400

    Try fixing osx again

--- setup.py
+++ setup.py
@@ -78,7 +78,8 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        darwin_opts = ['-mmacosx-version-min=10.7']
+        #['-stdlib=libc++', '-mmacosx-version-min=10.7']
         c_opts['unix'] += darwin_opts
         l_opts['unix'] += darwin_opts
 
