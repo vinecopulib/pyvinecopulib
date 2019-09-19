32c0061f6e1c65a2bc8cc9c76caedf5930fdd556
tnagler <thomas.nagler@tum.de>
2019-09-19 12:30:31 +0200

    libc++

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
-        darwin_opts = ['-mmacosx-version-min=10.7']
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
         #['-stdlib=libc++', '-mmacosx-version-min=10.7']
         c_opts['unix'] += darwin_opts
         l_opts['unix'] += darwin_opts
