f9f36a4514bc640db4a195980fa3411f6a0bb352
Thibault Vatter <thibault.vatter@gmail.com>
2019-11-06 15:37:37 -0500

    Being sneaky

--- setup.py
+++ setup.py
@@ -111,9 +111,17 @@ class BuildExt(build_ext):
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+
+def local_scheme(version):
+    """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
+    to be able to upload to Test PyPI"""
+
+    return ""
+
+
 setup(
     name='pyvinecopulib',
-    use_scm_version=True,
+    use_scm_version={"local_scheme": local_scheme},
     setup_requires=['setuptools_scm'],
     install_requires=requirements,
     author='Thomas Nagler and Thibault Vatter',
