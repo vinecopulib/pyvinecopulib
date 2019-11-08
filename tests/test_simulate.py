b8d1311ead19a37bb19f52cbde75a9255ad42f8b
Thibault Vatter <thibault.vatter@gmail.com>
2019-11-07 19:12:34 -0500

    Move CI to GitHub actions (#39)
    * remove travis/appveyor
    
    * Add continuous integration action
    
    * Exclude lib from flake8 check
    
    * Only test our stuff
    
    * Add multi-platform
    
    * Move back to .github/workflows
    
    * Typo
    
    * Forgot the run-on
    
    * Remove /dev/null for windows
    
    * Add json stuff
    
    * Test with --find-link
    
    * Try windows more
    
    * Add more windows stuff
    
    * cmd instead of call?
    
    * Conditiona VSCP tool
    
    * Test conditional again
    
    * Test contains again
    
    * Remove python 2.7
    
    * Remove windows-latest
    
    * Retry with windows-latest
    
    * Test with -vcvars_ver=14.0
    
    * Re-add all platforms
    
    * cleanup
    
    * Add PyPI
    
    * Remove all builds except one
    
    * Being sneaky
    
    * Better requirements
    
    * Actions conda (#40)
    
    * Test conda
    
    * Remove python setup
    
    * Re-add Python
    
    * Ubuntu only for now
    
    * Add conda to path
    
    * Add conda to path
    
    * Add setuptools_scp to meta.yaml
    
    * Re-add macOS
    
    * Try a fix for macOS
    
    * Do sudo conda update
    
    * try fix the stupid macOS issue
    
    * Try macos with new permission
    
    * Try the stupid sudo everywhere
    
    * Test with new miniconda install
    
    * Correct typo
    
    * Try windows
    
    * Test install on mac only (ubuntu should work)
    
    * remove fail-fast
    
    * Add proper environment variables
    
    * Try again install
    
    * Stop being stupid
    
    * Proper path
    
    * Remove ubuntu from x86 architecture
    
    * Clean without windows working
    
    * Actions conda (#41)
    
    * Test nvoke-WebRequest
    
    * Install miniconda
    
    * Properly install miniconda for windows
    
    * dfhds
    
    * Test install with Start-Process
    
    * Add to path
    
    * Try with ArgumentsList
    
    * Stop being stupid
    
    * Try without echo
    
    * Try with backslash?
    
    * Add miniconda and scripts to path
    
    * Try again to add stuff to path
    
    * Test adding another way
    
    * Switch back
    
    * Try again :/
    
    * try adding bin to path@
    
    * echo path
    
    * manually set path
    
    * syntax fix
    
    * check path in other actino
    
    * separate steps
    
    * hardcode /home/n5
    
    * let's see
    
    * is it even there?
    
    * cmd
    
    * hard code install path
    
    * ..
    
    * why is it not installing
    
    * unsilence
    
    * fail on purpose
    
    * different path
    
    * wrong slash
    
    * no path
    
    * check C
    
    * more dirs
    
    * seems like C:\Miniconda
    
    *  try activate.bat instead of path
    
    * typo
    
    * PATH + activate conda/issues/8836
    
    * conda's just a gigantic mess
    
    * the path want's some more
    
    * conda init somewhere else
    
    * compiler('cxx') req
    
    * no path
    
    * wrong activate path
    
    * why not
    
    * why did I think offical conda solutions work
    
    * don't activate?
    
    * show me dirs
    
    * reduce paths
    
    * hide dirs
    
    * Try with proper VS version
    
    * Try distutils stuff for python 3.5 on windows
    
    * Try again
    
    * Remove python 3.5 again
    
    * Add python 3.5 back but exclude for windows
    
    * Try to install from the binaries
    
    * Publish from a second job
    
    * Correct publish stuff
    
    * Upload binaries
    
    * Add wheel install
    
    * Make the publish job conditional on build
    
    * Correct artifact name
    
    * Remove unnecessary rms
    
    * Comment to stuff to understand what's going on
    
    * Properly rename linux files
    
    * Try with linux AND macos
    
    * Add windows and pray
    
    * Clean pypi workflow
    
    * Clean pypi workflow
    
    * Remove windows from conda
    
    * Clean conda stuff
    
    * Switch back to build instead of dist
    
    * Reverse to old version that was working
    
    * Update submodules
    
    * Remove compiler from recipe
    
    * Bring updates from the shipping branch
    
    * Try noarch
    
    * Add .eggs to .gitignore
    
    * Remove the generic thing
    
    * Remove all conda stuff
    
    * Try something with test pypi (#43)
    
    * Testssomething (#44)
    
    * Try something with test pypi
    
    * Deactivate the push to test pypi temporarily

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,11 +1,12 @@
 import pyvinecopulib as pv
 import numpy as np
 
-def test_version():
-  assert pv.__version__ == '0.0.1'
+#  def test_version():
+#  assert pv.__version__ == '0.0.1'
+
 
 def test_simulate_uniform():
-  assert isinstance(
-    pv.simulate_uniform(10, 2, False, [1, 2]),
-    np.ndarray
-  )
+    assert isinstance(
+        pv.simulate_uniform(10, 2, False, [1, 2]),
+        np.ndarray
+    )
