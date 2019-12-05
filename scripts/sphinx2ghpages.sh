82c7c10b39806f01e5b35ec124b247ce16b6b828
Thibault Vatter <thibault.vatter@gmail.com>
2019-12-05 18:30:11 -0500

    script for the doc

---
+++ scripts/sphinx2ghpages.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+doc_dictory=docs/_build
+branch_name=gh-pages
+
+# get a clean dev branch
+git checkout dev 
+git pull origin dev 
+git clean -df
+git checkout -- .
+git fetch --all
+
+# install the library
+pip install .
+
+# build html docs from sphinx files
+python3 docs/serve_sphinx.py
+
+# create or use orphaned gh-pages branch
+if [ $(git branch --list "$branch_name") ]
+then
+	git stash
+	git checkout $branch_name
+	git pull origin $branch_name
+	#git stash apply
+	git checkout stash -- . # force git stash to overwrite added files
+else
+	git checkout --orphan "$branch_name"
+fi
+
+if [ -d "$doc_directory" ]
+then
+	ls | grep -v _build | xargs rm -r
+	mv _build/* . && rm -rf _build
+	git add .
+	git commit -m "new pages version $(date)"
+	git push origin gh-pages
+	# github.com recognizes gh-pages branch and create pages
+	# url scheme https//:[github-handle].github.io/[repository]
+else
+	echo "directory $doc_directory does not exists"
+fi
