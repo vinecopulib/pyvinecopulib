276bd44688084da63adedd424107c57d5ff397f8
Thibault Vatter <thibault.vatter@gmail.com>
2024-06-07 11:16:00 +0200

    nightmare is starting

--- setup.py
+++ setup.py
@@ -1,40 +1,18 @@
 import os
-import subprocess
 from pathlib import Path
-from sys import platform
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 
 def find_include_dirs():
-  if platform == "linux" or platform == "linux2" or platform == "darwin":
-    try:
-      # Run CMake and capture its output
-      cmake_output = subprocess.check_output(
-        ["cmake", "."], stderr=subprocess.STDOUT, text=True
-      )
+  boost_include = os.environ.get("Boost_INCLUDE_DIR")
+  if not boost_include:
+    raise RuntimeError("Boost_INCLUDE_DIR environment variables must be set.")
 
-      # Extract the include directories from CMake output
-      for line in cmake_output.splitlines():
-        if line.startswith("-- EIGEN3_INCLUDE_DIRS="):
-          eigen_include = line.split("=")[1].strip()
-        elif line.startswith("-- Boost_INCLUDE_DIRS="):
-          boost_include = line.split("=")[1].strip()
-
-    except subprocess.CalledProcessError as e:
-      raise RuntimeError(
-        f"Could not find Boost or Eigen using CMake. Ensure they are installed. Error: {e.output}"
-      )
-
-  elif platform == "win32":
-    boost_include = os.environ.get("BOOST_INCLUDE_DIR")
-    eigen_include = os.environ.get("EIGEN_INCLUDE_DIR")
-
-    if not boost_include or not eigen_include:
-      raise RuntimeError(
-        "BOOST_INCLUDE_DIR and EIGEN_INCLUDE_DIR environment variables must be set."
-      )
+  eigen_include = os.environ.get("EIGEN3_INCLUDE_DIR")
+  if not eigen_include:
+    raise RuntimeError("EIGEN3_INCLUDE_DIR environment variables must be set.")
 
   include_dirs = [
     boost_include,
