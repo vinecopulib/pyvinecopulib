743fa8d531754626cc4a1cbeeeccf92fc2717b5e
Thibault Vatter <thibault.vatter@gmail.com>
2024-06-10 14:55:54 +0200

    Remove Eigen and boost sources from the repo (#132)
    * Remove eigen submodule as well as boost copy and rely on system install
    
    * nightmare is starting
    
    * macos-14 fix using venv
    
    * try again
    
    * try again+1
    
    * try again+2
    
    * try again+3
    
    * try again+4
    
    * try again+5
    
    * try again+6
    
    * try again+7
    
    * try again+8
    
    * try again+9
    
    * try again+10
    
    * try again+11
    
    * try again+12
    
    * try again+13
    
    * try again+14
    
    * try again+15
    
    * try again+16
    
    * try again+17
    
    * try again+18
    
    * try again+19
    
    * try again+20
    
    * try again+21
    
    * try again+22
    
    * try again+23
    
    * try again+24
    
    * try again+25
    
    * try again+26
    
    * try again+27
    
    * try again+28
    
    * try again+29
    
    * try again+30
    
    * try again+31
    
    * try again+32
    
    * try again+33
    
    * try again+34

--- setup.py
+++ setup.py
@@ -1,32 +1,50 @@
 import os
-import tarfile
-from glob import glob
 from pathlib import Path
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-# Extract boost if not already done.
-if not os.path.isdir('lib/boost'):
-  boost_archive = glob(os.path.join('lib', 'boost*.tar.gz'))[0]
-  tar = tarfile.open(boost_archive)
-  tar.extractall(path='lib/boost')
-  tar.close()
+
+def find_include_dirs():
+  boost_include = os.environ.get("Boost_INCLUDE_DIR")
+  if not boost_include:
+    raise RuntimeError("Boost_INCLUDE_DIR environment variables must be set.")
+
+  eigen_include = os.environ.get("EIGEN3_INCLUDE_DIR")
+  if not eigen_include:
+    raise RuntimeError("EIGEN3_INCLUDE_DIR environment variables must be set.")
+
+  # Check if directories exist and are accessible
+  if not os.path.isdir(boost_include):
+    raise RuntimeError(f"Boost include directory not found: {boost_include}")
+  if not os.path.isdir(eigen_include):
+    raise RuntimeError(f"Eigen include directory not found: {eigen_include}")
+
+  include_dirs = [
+    boost_include,
+    eigen_include,
+  ]  # , os.path.join(eigen_include, "unsupported")]
+
+  return include_dirs
+
+
+include_dirs = find_include_dirs()
+include_dirs.append("lib/vinecopulib/include")
+include_dirs.append("lib/wdm/include")
 
 setup(
-    name='pyvinecopulib',
-    long_description=(Path('setup.py').parent / 'README.md').read_text(),
-    long_description_content_type='text/markdown',
-    ext_modules=[
-        Pybind11Extension('pyvinecopulib', ['src/main.cpp'],
-                          include_dirs=[
-                              'lib/boost', 'lib/eigen',
-                              'lib/eigen/unsupported',
-                              'lib/vinecopulib/include', 'lib/wdm/include'
-                          ],
-                          language='c++',
-                          cxx_std=17)
-    ],
-    cmdclass={'build_ext': build_ext},
-    zip_safe=False,
+  name="pyvinecopulib",
+  long_description=(Path("setup.py").parent / "README.md").read_text(),
+  long_description_content_type="text/markdown",
+  ext_modules=[
+    Pybind11Extension(
+      "pyvinecopulib",
+      ["src/main.cpp"],
+      include_dirs=include_dirs,
+      language="c++",
+      cxx_std=17,
+    )
+  ],
+  cmdclass={"build_ext": build_ext},
+  zip_safe=False,
 )
