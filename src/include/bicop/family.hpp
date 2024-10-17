17e86c007b8aac7a8fc1324ee110fc18acb03cd9
Thibault Vatter <thibault.vatter@gmail.com>
2024-10-17 15:04:14 +0200

    Tawn family

--- src/include/bicop/family.hpp
+++ src/include/bicop/family.hpp
@@ -29,14 +29,27 @@ init_bicop_family(py::module_& module)
      (``gaussian``, ``clayton``, ``gumbel``, ``frank``, and ``joe``),
    - ``two_par`` contains the parametric families with two parameters
      (``student``, ``bb1``, ``bb6``, ``bb7``, and ``bb8``),
-   - ``elliptical`` contains the elliptical families,
-   - ``archimedean`` contains the archimedean families,
-   - ``bb`` contains the BB families,
+   - ``three_par`` contains the parametric families with three parameters
+     (``tawn``),
+   - ``elliptical`` contains the elliptical families (``gaussian`` and
+     ``student``),
+   - ``archimedean`` contains the archimedean families (``clayton``,
+     ``gumbel``, ``frank``, ``joe``, ``bb1``, ``bb6``, ``bb7``, and ``bb8``),
+   - ``extreme_value`` contains the extreme value families (``tawn`` and
+     ``gumbel``),
+   - ``bb`` contains the BB families (``bb1``, ``bb6``, ``bb7``, and ``bb8``),
    - ``itau`` families for which estimation by Kendall's tau inversion is
      available (``indep``, ``gaussian``, ``student``, ``clayton``,
      ``gumbel``, ``frank``, ``joe``),
-   - ``lt`` contains the families that are lower-tail dependent,
-   - ``ut`` contains the families that are upper-tail dependent.
+   - ``lt`` contains the families that are lower-tail dependent (``clayton``,
+     ``bb1``, ``bb7``,  ``tawn``),
+   - ``ut`` contains the families that are upper-tail dependent (``gumbel``,
+     ``joe``, ``bb1``, ``bb6``, ``bb7``, ``bb8``, ``tawn``),
+   - ``flip_by_rotation`` contains families that can be flipped by rotation 
+     (``clayton``, ``gumbel``, ``frank`` , ``joe``, ``bb1``, ``bb6``, ``bb7``, ``bb8``, ``tawn``),
+   - ``rotationless`` contains families that don't have a rotation 
+     because they already cover positive and negative dependence
+      (``indep``, ``gaussian``, ``student``, ``frank``, ``tll``).
    )pbdoc")
     .value("indep", BicopFamily::indep, bicopfamily_doc.indep.doc)
     .value("gaussian", BicopFamily::gaussian, bicopfamily_doc.gaussian.doc)
@@ -49,6 +62,7 @@ init_bicop_family(py::module_& module)
     .value("bb6", BicopFamily::bb6, bicopfamily_doc.bb6.doc)
     .value("bb7", BicopFamily::bb7, bicopfamily_doc.bb7.doc)
     .value("bb8", BicopFamily::bb8, bicopfamily_doc.bb8.doc)
+    .value("tawn", BicopFamily::tawn, bicopfamily_doc.tawn.doc)
     .value("tll", BicopFamily::tll, bicopfamily_doc.tll.doc)
     .export_values();
 
@@ -57,10 +71,14 @@ init_bicop_family(py::module_& module)
   module.attr("nonparametric") = bicop_families::nonparametric;
   module.attr("one_par") = bicop_families::one_par;
   module.attr("two_par") = bicop_families::two_par;
+  module.attr("three_par") = bicop_families::three_par;
   module.attr("elliptical") = bicop_families::elliptical;
   module.attr("archimedean") = bicop_families::archimedean;
+  module.attr("extreme_value") = bicop_families::extreme_value;
   module.attr("bb") = bicop_families::bb;
+  module.attr("rotationless") = bicop_families::rotationless;
   module.attr("lt") = bicop_families::lt;
   module.attr("ut") = bicop_families::ut;
   module.attr("itau") = bicop_families::itau;
+  module.attr("flip_by_rotation") = bicop_families::flip_by_rotation;
 }
