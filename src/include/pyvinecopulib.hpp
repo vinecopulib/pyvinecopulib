1a843053eedad53cf8de7eedf857d06d9bf356dc
Thibault Vatter <thibault.vatter@gmail.com>
2024-12-10 01:54:12 +0100

    windows preprocessor

--- src/include/pyvinecopulib.hpp
+++ src/include/pyvinecopulib.hpp
@@ -1,3 +1,7 @@
+#if defined(_MSC_VER) && _MSC_VER < 1500 // VC++ 8.0 and below
+#define snprintf _snprintf
+#endif
+
 #include "bicop/class.hpp"
 #include "bicop/family.hpp"
 #include "bicop/fit_controls.hpp"
