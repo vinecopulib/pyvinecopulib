fa7695aa139906d5f1ad209ae516343935146b32
Thibault Vatter <thibault.vatter@gmail.com>
2025-08-08 22:45:07 +0200

    reschuffled

--- examples/forests/vinesforest/vines_forest.py
+++ examples/forests/vinesforest/vines_forest.py
@@ -1,6 +1,5 @@
-from typing import Optional
-
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 import pyvinecopulib as pv
 
@@ -11,13 +10,16 @@ class VinesForest:
   def __init__(
     self,
     n_vines: int = 100,
+    train_val_split: float = 0.8,
     algorithm: str = "jck",
     controls: pv.FitControlsVinecop = pv.FitControlsVinecop(
       family_set=[pv.tll], num_threads=1
     ),
+    seed: int = 42,
   ) -> None:
     self.n_vines = n_vines
     self.algorithm = algorithm
+    self.train_val_split = train_val_split
     if algorithm not in ALGORITHMS:
       raise ValueError(f"Algorithm must be one of {ALGORITHMS}")
     if algorithm == "wilson":
@@ -28,48 +30,66 @@ class VinesForest:
     #   warnings.warn(
     #     "num_threads > 1 is not supported by VinesForest. Setting num_threads=1."
     #   )
-    self.vines: list[tuple[int, pv.Vinecop]] = []
+    self.seed = seed
 
   def fit(
     self,
     data: np.ndarray,
-    trunc_lvl: int = int(2**31 - 1),
-    seed: int = 42,
   ) -> None:
-    # Adjust and set trunc_lvl
-    dim = data.shape[1]
-    self.controls.trunc_lvl = min(dim, trunc_lvl)
-
     # Set seed and create random number generator
-    rng = np.random.default_rng(seed)
+    rng = np.random.default_rng(self.seed)
 
     # For the results
     self.vines = []
+    logliks_forest = []
 
-    # These seeds ensure that the sampled vines are different
-    seeds = [int(rng.integers(0, 2**31 - 1)) for _ in range(self.n_vines)]
+    # Save the initial control seed
+    initial_controls_seed = self.controls.seeds
 
-    for seed_i in seeds:
-      rng = np.random.default_rng(seed_i)
-      seeds_i = [int(rng.integers(0, 2**31 - 1)) for _ in range(10)]
-      if self.algorithm == "jck":
-        structure = pv.RVineStructure.simulate(data.shape[1], seeds=seeds_i)
-        vine = pv.Vinecop.from_data(
-          data=data, structure=structure, controls=self.controls
-        )
-      else:
-        self.controls.seeds = seeds_i
-        vine = pv.Vinecop.from_data(data=data, controls=self.controls)
-      self.vines.append((seed_i, vine))
+    train, val = train_test_split(
+      data, train_size=self.train_val_split, random_state=self.seed
+    )
+    self.vine_dissman = pv.Vinecop.from_data(data=train, controls=self.controls)
+    self.loglik_dissman = np.log(self.vine_dissman.pdf(val))
 
-  def loglik(
-    self, data: Optional[np.ndarray] = None, per_observation: bool = False
-  ) -> np.ndarray:
-    if not self.vines:
-      raise ValueError("VinesForest not fitted yet")
-    if data is None:
-      data = np.ndarray(shape=(0, 0), dtype=np.float64)
-    if not per_observation:
-      return np.array([vine.loglik(data) for _, vine in self.vines])
-    else:
-      return np.concatenate([vine.pdf(data) for _, vine in self.vines]).log()
+    for _ in range(self.n_vines):
+      # Seeding
+      seeds = [int(rng.integers(0, 2**31 - 1)) for _ in range(2)]
+      self.controls.seeds = seeds
+
+      # If the algorithm is jck, we simulate the structure
+      structure = (
+        pv.RVineStructure.simulate(data.shape[1], seeds=seeds)
+        if self.algorithm == "jck"
+        else None
+      )
+
+      # Fit the vine
+      vine = pv.Vinecop.from_data(
+        data=train, structure=structure, controls=self.controls
+      )
+
+      # Scoring per observation
+      loglik_vine = np.log(vine.pdf(val))
+
+      # Saving the vine and its per observation validation loglikelihood
+      self.vines.append(vine)
+      logliks_forest.append(loglik_vine)
+
+    # Store the loglikelihoods of the forest
+    self.logliks_forest = np.array(logliks_forest)
+
+    # Restore the initial control seed
+    self.controls.seeds = initial_controls_seed
+
+  # def loglik(
+  #   self, data: Optional[np.ndarray] = None, per_observation: bool = False
+  # ) -> np.ndarray:
+  #   if not self.vines:
+  #     raise ValueError("VinesForest not fitted yet")
+  #   if data is None:
+  #     data = np.ndarray(shape=(0, 0), dtype=np.float64)
+  #   if not per_observation:
+  #     return np.array([vine.loglik(data) for vine in self.vines])
+  #   else:
+  #     return np.log(np.concatenate([vine.pdf(data) for vine in self.vines]))
